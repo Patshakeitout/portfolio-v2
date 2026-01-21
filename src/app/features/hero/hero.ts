c8dc8f7387ad19995b37f27faae74a4d2407536b
Patrick <patrick_schauer@t-online.de>
2026-01-22 00:07:48 +0100

    Initial commit of Portfolio V2 with folder structure

---
+++ src/app/features/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../core/components/navbar/navbar';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss',
+})
+export class Hero {
+
+}
