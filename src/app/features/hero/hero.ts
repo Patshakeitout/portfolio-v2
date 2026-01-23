3cce6780794efafa4f3b6f59796988afdc2d7e9d
Patrick <patrick_schauer@t-online.de>
2026-01-23 22:27:26 +0100

    feat: header with logo and menu hamburger

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 
+import { Header } from '../../core/layout/header/header';
+
 @Component({
   selector: 'app-hero',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, Header],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
